4b61cd5cb9b4894d018191087b56e5ab8ea4ece3
haleli amiad <haleli.am@gmail.com>
2021-01-07 18:16:43 +0100

    deploy

---
+++ precache-manifest.688e6a8ef18ece62d2365cd541aef048.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2415b15f2f6e131377bb",
+    "url": "/youdoob/css/app.7b5f33d8.css"
+  },
+  {
+    "revision": "4c776b4332f404a553812af27853395b",
+    "url": "/youdoob/fonts/Assistant-ExtraLight.4c776b43.ttf"
+  },
+  {
+    "revision": "f049695a132a8d043efbf32fe5f6db2c",
+    "url": "/youdoob/fonts/LeagueGothic-Regular.f049695a.ttf"
+  },
+  {
+    "revision": "5506c7c1b78f12f1aa9d57c9b4ea6d08",
+    "url": "/youdoob/img/youtube-logo.5506c7c1.png"
+  },
+  {
+    "revision": "05a9baa9c8a9b45e6ff639af265fd5d0",
+    "url": "/youdoob/index.html"
+  },
+  {
+    "revision": "2415b15f2f6e131377bb",
+    "url": "/youdoob/js/app.df62d49f.js"
+  },
+  {
+    "revision": "0854b35104f557845b47",
+    "url": "/youdoob/js/chunk-vendors.177d7fe2.js"
+  },
+  {
+    "revision": "a099817666b25410550eead0381b32de",
+    "url": "/youdoob/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/youdoob/robots.txt"
+  }
+]);
